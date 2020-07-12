defbbd6c7d6fc3fc2b083f201e192ba8b5528275
kindy <kinti462@gmail.com>
2020-07-12 16:08:57 +0300

    Added class Ellepse and Circle and overridden methods for calculating the area and perimeter.

---
+++ Task1/Task1.2/Figures/Circle.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Figures
+{
+    public class Circle : Figure
+    {
+        public double Radius { get; set; }
+        public Circle(double radius)
+        {
+            Radius = radius;
+        }
+
+        public override double CalcP()
+        {
+            return 2 * Math.PI * Radius;
+        }
+
+        public override double CalcS()
+        {
+            return Math.PI * Radius * Radius;
+        }
+
+        public override string ToString()
+        {
+            return "Circle : R = " + Convert.ToString(Radius) + "S = " + Convert.ToString(CalcS()) + "P = " + Convert.ToString(CalcP());
+        }
+    }
+}
---
+++ Task1/Task1.2/Figures/Ellipse.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Figures
+{
+    public class Ellipse : Figure
+    {
+        public double DiagonalA { get; set; }
+        public double DiagonalB { get; set; }
+        public Ellipse(double diagonalA, double diagonalB)
+        {
+            DiagonalA = diagonalA;
+            DiagonalB = diagonalB;
+        }
+
+        public override double CalcP()
+        {
+            return 4 * (Math.PI * DiagonalA * DiagonalB + (DiagonalA - DiagonalB)) / (DiagonalA + DiagonalB);
+        }
+
+        public override double CalcS()
+        {
+            return Math.PI * DiagonalA * DiagonalB;
+        }
+
+        public override string ToString()
+        {
+            return "Ellipse : DiagonalA = " + Convert.ToString(DiagonalA) + "DiagonalB = " + Convert.ToString(DiagonalB) +
+                " S = " + Convert.ToString(CalcS()) + " P = " + Convert.ToString(CalcP());
+        }
+    }
+}
