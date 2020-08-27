6347d002e7ac97e5b896f1f710336f4447d44bb5
kindy <kinti462@gmail.com>
2020-08-27 21:49:11 +0300

    Added MSSQLGroupDAO class.

--- Task6/University/MSSQLGroupDAO.cs
+++ Task6/University/MSSQLGroupDAO.cs
@@ -4,65 +4,63 @@ using System.Data.SqlClient;
 namespace University
 {
     /// <summary>
-    /// DAO grades for MS SQL Server DBMS.
+    /// DAO groups for MS SQL Server DBMS.
     /// </summary>
-    public class MSSQLGradesDAO : IGrade
+    public class MSSQLGroupDAO : IGroup
     {
         /// <summary>
         /// SQL query to add data to the database.
         /// </summary>
         private const string INSERT_EXPRESSION =
-            "INSERT INTO Grades(ExamId, StudentId, Grade) VALUES (@examId, @studentId, @grade)";
+            "INSERT INTO Groups(GroupName) VALUES (@groupName)";
 
         /// <summary>
-        /// SQL query to get the list of grades from the database.
+        /// SQL query to get the list of groups from the database.
         /// </summary>
-        private const string GET_GRADE_INFORMATION_EXPRESSION
-            = "SELECT * FROM Grades";
+        private const string GET_GROUP_INFORMATION_EXPRESSION
+            = "SELECT * FROM Groups";
 
         /// <summary>
         /// SQL query to update data in the database.
         /// </summary>
         private const string UPDATE_EXPRESSION
-            = "UPDATE Grades SET ExamId=@examId, StudentId=@studentId, Grade=@grade WHERE GradeId=@gradeId";
+            = "UPDATE Groups SET GroupName=@groupName WHERE GroupId=@groupId";
 
         /// <summary>
         /// SQL query to delete data from the database.
         /// </summary>
         private const string DELETE_EXPRESSION
-            = "DELETE FROM Grades WHERE GradeId=@gradeId";
+            = "DELETE FROM Groups WHERE GroupId=@groupId";
 
         /// <summary>
-        /// Obtaining an grade ID.
+        /// Obtaining an group ID.
         /// </summary>
-        private const string GET_GRADE_EXPRESSION
-            = "SELECT GradeId FROM Grades WHERE ExamId=@examId AND StudentId=@studentId AND Grade=@grade";
+        private const string GET_GROUP_EXPRESSION
+            = "SELECT GroupId FROM Groups WHERE GroupName=@groupName";
 
 
         private string connectionString;
 
 
         /// <summary>
-        /// Creation of DAO grades for MS SQL Server.
+        /// Creation of DAO groups for MS SQL Server.
         /// </summary>
         /// <param name="connectionString">The string connecting to the database.</param>
-        public MSSQLGradesDAO(string connectionString)
+        public MSSQLGroupDAO(string connectionString)
         {
             this.connectionString = connectionString;
 
             FactoryDAO factory = FactoryDAO.GetFactoryDAO(FactoryDAO.DBMS.MSSQL, connectionString);
         }
 
-        private int GetIdGrade(Grades grade)
+        private int GetIdGroup(Group group)
         {
             int id = 0;
             using (SqlConnection sqlConnection = new SqlConnection(connectionString))
             {
                 sqlConnection.Open();
-                SqlCommand sqlCommand = new SqlCommand(GET_GRADE_EXPRESSION, sqlConnection);
-                sqlCommand.Parameters.Add(new SqlParameter("@examId", grade.ExamId));
-                sqlCommand.Parameters.Add(new SqlParameter("@studentId", grade.StudentId));
-                sqlCommand.Parameters.Add(new SqlParameter("@grade", grade.Grade));
+                SqlCommand sqlCommand = new SqlCommand(GET_GROUP_EXPRESSION, sqlConnection);
+                sqlCommand.Parameters.Add(new SqlParameter("@groupName", group.GroupName));
                 SqlDataReader reader = sqlCommand.ExecuteReader();
                 if (reader.HasRows)
                 {
@@ -73,31 +71,27 @@ namespace University
             return id;
         }
 
-        private Grades CreateGrades(SqlDataReader reader)
+        private Group CreateGroup(SqlDataReader reader)
         {
-            Grades grade = new Grades();
+            Group group = new Group();
 
-            grade.ExamId = reader.GetInt32(1);
-            grade.StudentId = reader.GetInt32(2);
-            grade.Grade = reader.GetInt32(3);
-            return grade;
+            group.GroupName = reader.GetString(1);
+            return group;
         }
 
         /// <summary>
-        /// Adding a grade to the MS SQL Server database.
+        /// Adding a group to the MS SQL Server database.
         /// </summary>
-        /// <param name="grade">Grade.</param>
+        /// <param name="group">Group.</param>
         /// <returns>True if successful, otherwise False.</returns>
-        public bool Insert(Grades grade)
+        public bool Insert(Group group)
         {
             int numb;
             using (SqlConnection sqlConnection = new SqlConnection(connectionString))
             {
                 sqlConnection.Open();
                 SqlCommand sqlCommand = new SqlCommand(INSERT_EXPRESSION, sqlConnection);
-                sqlCommand.Parameters.Add(new SqlParameter("@subjectName", grade.ExamId));
-                sqlCommand.Parameters.Add(new SqlParameter("@examDate", grade.StudentId));
-                sqlCommand.Parameters.Add(new SqlParameter("@groupId", grade.Grade));
+                sqlCommand.Parameters.Add(new SqlParameter("@groupName", group.GroupName));
 
                 numb = sqlCommand.ExecuteNonQuery();
             }
@@ -105,66 +99,64 @@ namespace University
         }
 
         /// <summary>
-        /// Getting a list of all grades from the MS SQL Server database.
+        /// Getting a list of all groups from the MS SQL Server database.
         /// </summary>
-        /// <returns>All grades in the database.</returns>
-        public Grades[] GetGrades()
+        /// <returns>All groups in the database.</returns>
+        public Group[] GetGroups()
         {
-            List<Grades> grades = new List<Grades>();
+            List<Group> group = new List<Group>();
             using (SqlConnection connection = new SqlConnection(connectionString))
             {
                 connection.Open();
                 SqlCommand command = new SqlCommand(
-                    GET_GRADE_INFORMATION_EXPRESSION,
+                    GET_GROUP_INFORMATION_EXPRESSION,
                     connection);
 
                 SqlDataReader reader = command.ExecuteReader();
                 if (reader.HasRows)
                     while (reader.Read())
-                        grades.Add(CreateGrades(reader));
+                        group.Add(CreateGroup(reader));
             }
-            return grades.ToArray();
+            return group.ToArray();
         }
 
         /// <summary>
-        /// Updating the grade in the MS SQL Server database.
+        /// Updating the group in the MS SQL Server database.
         /// </summary>
-        /// <param name="nowGrade">Grade for renewal.</param>
-        /// <param name="newGrade">New grade.</param>
+        /// <param name="nowGroup">Group for renewal.</param>
+        /// <param name="newGroup">New group.</param>
         /// <returns>True if successful, otherwise False.</returns>
-        public bool Update(Grades nowGrade, Grades newGrade)
+        public bool Update(Group nowGroup, Group newGroup)
         {
             int numb;
 
             using (SqlConnection sqlConnection = new SqlConnection(connectionString))
             {
-                int id = GetIdGrade(nowGrade);
+                int id = GetIdGroup(nowGroup);
 
                 sqlConnection.Open();
                 SqlCommand sqlCommand = new SqlCommand(UPDATE_EXPRESSION, sqlConnection);
-                sqlCommand.Parameters.Add(new SqlParameter("@subjectName", newGrade.ExamId));
-                sqlCommand.Parameters.Add(new SqlParameter("@examDate", newGrade.StudentId));
-                sqlCommand.Parameters.Add(new SqlParameter("@groupId", newGrade.Grade));
+                sqlCommand.Parameters.Add(new SqlParameter("@groupName", newGroup.GroupName));
                 numb = sqlCommand.ExecuteNonQuery();
             }
             return numb > 0;
         }
 
         /// <summary>
-        /// Removing a grade from the MS SQL Server database.
+        /// Removing a group from the MS SQL Server database.
         /// </summary>
-        /// <param name="grade">Grade.</param>
+        /// <param name="group">Group.</param>
         /// <returns>True if successful, otherwise False.</returns>
-        public bool Delete(Grades grade)
+        public bool Delete(Group group)
         {
             int numb;
             using (SqlConnection sqlConnection = new SqlConnection(connectionString))
             {
-                int id = GetIdGrade(grade);
+                int id = GetIdGroup(group);
 
                 sqlConnection.Open();
                 SqlCommand sqlCommand = new SqlCommand(DELETE_EXPRESSION, sqlConnection);
-                sqlCommand.Parameters.Add(new SqlParameter("@gradeId", id));
+                sqlCommand.Parameters.Add(new SqlParameter("@groupId", id));
                 numb = sqlCommand.ExecuteNonQuery();
             }
             return numb > 0;
